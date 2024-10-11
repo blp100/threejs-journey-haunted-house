c018ae8f53a25861f4d33e6aff2b2a024dcc41f6
Po-Cheng Yeh <blp100x@hotmail.com>
2024-10-11 15:18:28 +0800

    feat: add graves

--- src/script.js
+++ src/script.js
@@ -79,6 +79,32 @@ bush4.position.set(-1, 0.05, 2.6);
 
 house.add(bush1, bush2, bush3, bush4);
 
+// Graves
+const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2);
+const graveMaterial = new THREE.MeshStandardMaterial();
+
+const graves = new THREE.Group();
+scene.add(graves);
+
+for (let i = 0; i < 30; i++) {
+  const angle = Math.random() * Math.PI * 2;
+  const radius = 4 + Math.random() * 3;
+  const x = Math.sin(angle) * radius;
+  const z = Math.cos(angle) * radius;
+
+  // Mesh
+  const grave = new THREE.Mesh(graveGeometry, graveMaterial);
+  grave.position.x = x;
+  grave.position.z = z;
+  grave.position.y = Math.random() * 0.4;
+
+  grave.rotation.x = (Math.random() - 0.5) * 0.4;
+  grave.rotation.y = (Math.random() - 0.5) * 0.4;
+
+  // Add to graves group
+  graves.add(grave);
+}
+
 /**
  * Lights
  */
