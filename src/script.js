7f7fdc58f1314494850809988697707dd1f465d4
Po-Cheng Yeh <blp100x@hotmail.com>
2024-10-11 23:53:38 +0800

    feat: add fog

--- src/script.js
+++ src/script.js
@@ -424,6 +424,11 @@ sky.material.uniforms["mieCoefficient"].value = 0.1;
 sky.material.uniforms["mieDirectionalG"].value = 0.95;
 sky.material.uniforms["sunPosition"].value.set(0.3, -0.038, -0.95);
 
+/**
+ * Fog
+ */
+// scene.fog = new THREE.Fog("#ff0000", 10, 13);
+scene.fog = new THREE.FogExp2("#02343f", 0.1);
 
 /**
  * Animate
